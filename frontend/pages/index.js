f85c2a10e2e71e9459571a23d4366c8c98ed3207
Samuel Henderson <samzi.henderson@gmail.com>
2022-10-01 14:47:42 -0400

    feature: syllabus upload frontend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,41 +7,64 @@ import SyllabiList from 'components/syllabiList'
 import Header from 'components/header'
 import { PlusIcon } from '@heroicons/react/20/solid'
 
-const uploadFileHandler = async (e) => {
-  let syllabiTemp = syllabi
-
-  try {
-    setUploading(true)
-
-    for (let i = 0; i < e.target.files.length; i += 1) {
-      const file = e.target.files[i]
-      const formData = new FormData()
-      formData.append('image', file)
-
-      const config = {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      }
-
-      const { data } = await axios.post('/api/upload', formData, config)
-
-      syllabiTemp.push(data)
-    }
-
-    setSyllabi(syllabiTemp)
-    setUploading(false)
-  } catch (error) {
-    console.error(error)
-    setUploading(false)
-  }
-}
-
-
 export default function Home() {
   const [syllabiCount, setSyllabiCount] = useState(0);
   const [syllabi, setSyllabi] = useState([]);
+  const [syllabiNames, setSyllabiNames] = useState([]);
+  const [uploading, setUploading] = useState(false)
+
+  const deleteSyllabus = async (e, syllabusName) => {
+    let syllabiTemp = syllabi.slice()
+    let syllabiNamesTemp = syllabiNames.slice()
+
+    const index = syllabiTemp.indexOf(e.target.value)
+    const nameIndex = syllabiNamesTemp.indexOf(syllabusName)
   
+    syllabiTemp.splice(index, 1)
+    syllabiNamesTemp.splice(nameIndex, 1)
+
+    setSyllabiNames(syllabiNamesTemp)
+    setSyllabi(syllabiTemp)
+  
+    const path = e.target.value
+
+    //await axios.delete(`/api/upload/delete/${path}`)
+  }
+
+  const uploadFileHandler = async (e) => {
+    let syllabiTemp = syllabi
+    let syllabiNamesTemp = syllabiNames
+
+    try {
+      setUploading(true)
+
+      for (let i = 0; i < e.target.files.length; i += 1) {
+        const file = e.target.files[i]
+        const formData = new FormData()
+        formData.append('syllabus', file)
+
+        const config = {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+
+        //const { data } = await axios.post('/api/upload', formData, config)
+        //syllabiTemp.push(data)
+
+        syllabiTemp.push(formData)
+
+        syllabiNamesTemp.push(file.name)
+      }
+
+      setSyllabi(syllabiTemp)
+      setSyllabiNames(syllabiNamesTemp)
+      setUploading(false)
+    } catch (error) {
+      console.error(error)
+      setUploading(false)
+    }
+  }
 
   return (
     <>
@@ -65,22 +88,25 @@ export default function Home() {
           <ul className='flex-auto overflow-y-auto divide-y divide-gray-200 px-6'>
             {
               //check if user has added syllabi
-              syllabi && syllabi.length > 0 && syllabi[0].length > 1 ? (
-                syllabi.map((imageItem) => (
-                  <li key={imageItem} className='flex py-6 space-x-6'>
-                    <img
-                      src={imageItem}
-                      alt={'Offer'}
-                      className='flex-none w-60 aspect-auto max-h-100 object-center object-cover bg-gray-200 rounded-md'
+              syllabi && syllabi.length > 0 && (
+                syllabi.map((syllabus,index) => (
+                  <li key={syllabiNames[index]} className='flex py-6 space-x-4'>
+                    <Image
+                      src={require('../images/fileImage.png')}
+                      alt={'Syllabus'}
+                      width={100}
+                      height={100}
+                      className='flex-none aspect-auto object-center object-cover bg-gray-200 rounded-md'
                     />
                     <div className='flex flex-col justify-between space-y-2'>
                       <div className='flex space-x-4'>
+                        <h1>{syllabiNames[index]}</h1>
                         <button
                           type='button'
                           className='text-sm font-medium text-mainTheme hover:text-mainTheme'
-                          value={imageItem}
-                          key={imageItem}
-                          onClick={deleteImage}
+                          value={syllabus}
+                          key={syllabus}
+                          onClick={e => deleteSyllabus(e, syllabiNames[index])}
                         >
                           Remove
                         </button>
@@ -88,21 +114,11 @@ export default function Home() {
                     </div>
                   </li>
                 ))
-              ) : (
-                //else show default offer image
-                <li key='1' className='flex py-6 space-x-6'>
-                  <img
-                    src={require('images/fileImage.png')}
-                    alt={'Default offer image'}
-                    className='flex-none w-60 aspect-auto max-h-100 object-center object-cover bg-gray-200 rounded-md'
-                  />
-                  <div className='flex flex-col justify-between space-y-2'></div>
-                </li>
               )
             }
         <form className='mt-4'>
             <svg
-            className="mx-auto h-12 w-12 text-gray-200"
+            className="mx-auto h-12 w-12 text-gray-200 mt-4"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -117,53 +133,17 @@ export default function Home() {
             />
           </svg>
 
+        <div className="mt-2 text-sm font-medium text-gray-200 text-center mb-6 content-center">
           <h3 className="mt-2 text-sm font-medium text-gray-200 text-center mb-6">Upload a syllabus</h3>             
-          <input type='file' onChange="" multiple className="mt-2 text-sm font-medium text-gray-200 text-center mb-6 content-center"/>
+          <input type='file' onChange={e =>{
+            setSyllabiCount(syllabiCount + 1);
+            uploadFileHandler(e);
+          }}
+           multiple className="mt-2 ml-24 text-sm font-medium text-gray-200 text-center mb-6 content-center"/>
+        </div>
         </form>
         </ul>
       </section>
-
-        <div className="text-center">
-
-        {(syllabiCount == 0) ? (
-          <>
-            <h1 className="mt-6 text-lg font-medium text-gray-300 mb-6">Get started by uploading a syllabus</h1>
-          </>
-        ) : (
-        <>
-          <SyllabiList/>
-        </>
-        )}
-
-        <svg
-          className="mx-auto h-12 w-12 text-gray-200"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            vectorEffect="non-scaling-stroke"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
-          />
-        </svg>
-
-        <h3 className="mt-2 text-sm font-medium text-gray-300">Upload a syllabus</h3>
-          <div className="mt-6">
-          <input type="file" name="file"/>
-
-            <button
-              type="button"
-              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-              Upload
-            </button>
-          </div>
-      </div>
 
       </main>
     </>
