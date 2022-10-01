f9c7d493f3a04ac5612c9e533ebc26268c13fe40
Matt Nappo <mattnappo@gmail.com>
2022-10-01 17:53:26 -0400

    feat: MongoDB core logic done

--- backend/api.py
+++ backend/api.py
@@ -11,15 +11,16 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/upload/")
-def upload_syllabus(user: str = Form(), syllabus: UploadFile = Form()):
+def upload_syllabus(user: str = Form(), course: str = Form(), syllabus: UploadFile = Form()):
     contents = syllabus.file.read()
-    state.store_syllabus(user, syllabus.filename, contents)
+    state.store_syllabus(user, course, syllabus.filename, contents)
+    state.print_db()
 
     ''' DO ML STUFF HERE '''
 
     return {"status": "ok"}
 
-@app.post("/upload_all/")
+@app.post("/upload_all/DEP/") # Deprecated
 def upload_syllabi(user: str = Form(), syllabi: List[UploadFile] = Form()):
     for syllabus in syllabi:
         upload_syllabus(user, syllabus)
@@ -32,4 +33,11 @@ def get_user(user: str):
 @app.get("/clear")
 def clear():
     state.clear()
+    return {"status": "ok"}
+
+
+# DEV ROUTES
+@app.post("/dev/set_summary/")
+def dev_set_summary(user: str, course: str, summary: str):
+    state.set_summary(user, course, summary)
     return {"status": "ok"}
