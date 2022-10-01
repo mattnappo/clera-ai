06c8fdf6ae045a41fe31a7281551a2630f1f887f
AleksaGavric <aleksagavric1@gmail.com>
2022-10-01 15:06:17 -0400

    feat: fastapi with summary

--- ai/main.py
+++ ai/main.py
@@ -1,31 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from dotenv import load_dotenv
-import os
-
-import openai
+import openai, os, PyPDF2
 
 load_dotenv()
 
 OPENAI_KEY = os.getenv('KEY')
-print(OPENAI_KEY)
 openai.api_key = OPENAI_KEY
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    gpt_prompt = "Correct this to standard English:\n\nShe no went to the market."
+prompts = ["What are the key points?", "List the times of all exams", "Who is class taugh by?"]
 
+def summarize(file_name):
+    reader = PyPDF2.PdfFileReader(os.path.join(os.getcwd(), file_name)).pages[0]
+    text = reader.extract_text()
+    
     response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt=gpt_prompt,
-    temperature=0.5,
-    max_tokens=256,
-    top_p=1.0,
-    frequency_penalty=0.0,
-    presence_penalty=0.0
+        model="text-davinci-002",
+        prompt=f"Summarize this syllabus:\n\n{text}",
+        temperature=0.4,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
     )
+    
+    return text, response['choices'][0]['text']
 
-    return {"message": response['choices'][0]['text']}
-
-print("hey")
+@app.post("/summarize")
+async def get_summary(file_name : Request):
+    req_info = await file_name.json()
+    text, summary = summarize(req_info["file"])
+    
+    return {
+        "status" : "SUCCESS",
+        "text" : text,
+        "summary" : summary
+    }
+    
