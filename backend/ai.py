4a14c158eea1cdf6a7e390f458b866c5ee53ec2b
Matt Nappo <mattnappo@gmail.com>
2022-10-02 02:11:38 -0400

    feat: naive qa

--- backend/ai.py
+++ backend/ai.py
@@ -50,7 +50,8 @@ async def summarize(text):
         max_tokens=150,
         top_p=1.0,
         frequency_penalty=0.0,
-        presence_penalty=0.0
+        presence_penalty=0.0,
+        stop=["\n\n"]
     )
 
     time.sleep(3)
